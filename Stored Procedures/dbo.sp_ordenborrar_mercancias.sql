fa11c1ee28c8747af934e5aa397b208b8e5e3a4c
Julio Herrera <jcherrera@bgcapitalgroup.mx>
2022-08-12 17:47:39 -0500

    Segundo commit desde sql server modificamos los stops facturables

--- Stored Procedures/dbo.sp_ordenborrar_mercancias.sql
+++ Stored Procedures/dbo.sp_ordenborrar_mercancias.sql
@@ -10,23 +10,38 @@ BEGIN
 DECLARE 
 @vi_seq1 int,
 @vi_seq2 int
-        DELETE FROM freightdetail WHERE 
-        stp_number in (SELECT stp_number FROM stops WHERE ORD_hdrnumber = @Ai_orden) and fgt_count = 0  
-		DELETE FROM freightdetail WHERE 
-        stp_number in (SELECT stp_number FROM stops WHERE ORD_hdrnumber = @Ai_orden) and cmd_code in ('UNKNOWN','MERGENER')
-		DELETE FROM freightdetail WHERE 
-        stp_number in (SELECT stp_number FROM stops WHERE ORD_hdrnumber = @Ai_orden) and fgt_height = 0;
+        --DELETE FROM freightdetail WHERE 
+        --stp_number in (SELECT stp_number FROM stops WHERE ORD_hdrnumber = @Ai_orden) and fgt_count = 0  
+		BEGIN
+		CREATE TABLE #nsecuencias(
+		id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+		stp_number int,
+		fgt_sequence int
+		)
+		END
+		DELETE freightdetail where stp_number in (select stp_number from stops where stp_event in (select abbr from eventcodetable where ect_billable = 'Y') and lgh_number in(
+        select lgh_number from legheader where ord_hdrnumber in (1192968))) and fgt_count = 0
+
+		DELETE freightdetail where stp_number in (select stp_number from stops where stp_event in (select abbr from eventcodetable where ect_billable = 'Y') and lgh_number in(
+        select lgh_number from legheader where ord_hdrnumber in (@Ai_orden))) and cmd_code in ('UNKNOWN','MERGENER')
+		--DELETE FROM freightdetail WHERE 
+  --      stp_number in (SELECT stp_number FROM stops WHERE ORD_hdrnumber = @Ai_orden) and cmd_code in ('UNKNOWN','MERGENER')
+		
+		DELETE freightdetail where stp_number in (select stp_number from stops where stp_event in (select abbr from eventcodetable where ect_billable = 'Y') and lgh_number in(
+        select lgh_number from legheader where ord_hdrnumber in (@Ai_orden)))and fgt_height = 0
+		--DELETE FROM freightdetail WHERE 
+  --      stp_number in (SELECT stp_number FROM stops WHERE ORD_hdrnumber = @Ai_orden) and fgt_height = 0;
 		--PASO 1 HACER EL SELECT E INSERTARLO EN UNA TABLA TEMPORAL
 		
-		INSERT INTO ##nsecuencias(stp_number,fgt_sequence)
-		select distinct(stp_number),min(fgt_sequence)  from freightdetail where stp_number in (select stp_number from stops where lgh_number in(
+		INSERT INTO #nsecuencias(stp_number,fgt_sequence)
+		select distinct(stp_number),min(fgt_sequence)  from freightdetail where stp_number in (select stp_number from stops where stp_event in (select abbr from eventcodetable where ect_billable = 'Y') and lgh_number in(
         select lgh_number from legheader where ord_hdrnumber in (@Ai_orden))) GROUP BY stp_number
 
 		DECLARE @i int = 1,@total int,@id int = 1,@stp_number int,@fgt_sequence int,@fgt_number int
-		SELECT @total = COUNT(*) FROM ##nsecuencias
+		SELECT @total = COUNT(*) FROM #nsecuencias
 		WHILE (@i <= @total)
 		 BEGIN
-		     SELECT @stp_number = stp_number, @fgt_sequence = fgt_sequence FROM ##nsecuencias WHERE id = @id
+		     SELECT @stp_number = stp_number, @fgt_sequence = fgt_sequence FROM #nsecuencias WHERE id = @id
 
 			 IF @fgt_sequence <> 1
 			 BEGIN 
