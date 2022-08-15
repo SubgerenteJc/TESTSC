a45c87c0db8f6227173fe0f33984c85bc0d8eef1
Julio Herrera <jcherrera@bgcapitalgroup.mx>
2022-08-15 15:18:53 -0500

    sp_NotificacionLiverded bodyC update from sql server JC

--- Stored Procedures/dbo.sp_NotificacionesLiverded.sql
+++ Stored Procedures/dbo.sp_NotificacionesLiverded.sql
@@ -2,11 +2,11 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-CREATE PROCEDURE [dbo].[sp_NotificacionesLiverded](@leg varchar(100),@titulo varchar(100),@mensaje varchar(200))
+CREATE PROCEDURE [dbo].[sp_NotificacionesLiverded](@leg varchar(100),@titulo varchar(100),@mensaje varchar(8000))
 	
 AS
 BEGIN
-DECLARE @bodyC varchar(1000)
+DECLARE @bodyC varchar(8000)
 DECLARE @subjectC varchar(1000)
 
 set @bodyC = @titulo + @leg + ' ' + 'Error: ' + @mensaje;
